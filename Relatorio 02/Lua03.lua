d4ae1645fccf26d3e4903f8be44fe3eae77b909f
Pedro Gabriel da Silva <p3dr0.tr@gmail.com>
2026-08-26 13:01:38 -0300

    formatei

--- Relatorio 02/Lua03.lua
+++ Relatorio 02/Lua03.lua
@@ -1,8 +1,8 @@
 function filtrarMaiores(tabela, limite)
     local tabelaDosMaiores = {}
-    for i = 1, #tabela do --vasculha a tabela atraz do(s) valore(s) maior(es) do que K  
+    for i = 1, #tabela do -- vasculha a tabela atraz do(s) valore(s) maior(es) do que K  
         if tabela[i] > limite then
-            table.insert(tabelaDosMaiores, tabela[i])--ta inserindo os valores maiores do que K em uma Tabela separada
+            table.insert(tabelaDosMaiores, tabela[i]) -- ta inserindo os valores maiores do que K em uma Tabela separada
         end
     end 
     return tabelaDosMaiores            
@@ -12,24 +12,24 @@ print("Digite a quantidade de elementos (N): ")
 local N = tonumber(io.read())
 local tabela = {}
 
-if N and N > 0 then--garente que eu estou entrando com valores validos 
+if N and N > 0 then -- garente que eu estou entrando com valores validos 
     
-    --entrando com os valores da tabela 
+    -- entrando com os valores da tabela 
     for i = 1, N do    
         print("Digite o elemento "..i..": ")
         local elemento = tonumber(io.read())
-        table.insert(tabela, elemento) --vai inserir o elemento na tabela 
+        table.insert(tabela, elemento) -- vai inserir o elemento na tabela 
     end
 
-    --inserindo o valor a ser comparado 
+    -- inserindo o valor a ser comparado 
     print("Digite o valor limite (K):  ")
     local K = tonumber(io.read())
     
-    if K then--garante que eu estou entrando com valores validos
+    if K then -- garante que eu estou entrando com valores validos
         
-        local resultado= filtrarMaiores(tabela, K)--puxa a função
+        local resultado= filtrarMaiores(tabela, K)
         print("--- Elementos maiores que "..K.." ---")
-        for i = 1, #resultado do--vasculha a tabela me devolvendo os valores da tabela 
+        for i = 1, #resultado do -- vasculha a tabela me devolvendo os valores da tabela 
             print(resultado[i])
         end    
     

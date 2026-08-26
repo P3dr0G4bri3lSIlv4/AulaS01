fb4f9e4ad418f4147b93dda85b29b9c855a28288
Pedro Gabriel da Silva <p3dr0.tr@gmail.com>
2026-08-26 12:55:50 -0300

    formatei algumas coisas

--- Relatorio 02/Lua 01.lua
+++ Relatorio 02/Lua 01.lua
@@ -1,21 +1,23 @@
 function gerarTabelaPotencias(inicio, fim, base)
     for i = inicio, fim do
-        local resultado = base ^ i --faz o calculo das potencias 
+        local resultado = base ^ i 
         print(base .. "^" .. i .. "=" .. resultado) 
     end
 end        
 
 print ("Digite o expoente inicial: ")
 local inicio = tonumber(io.read())
+
 print ("Digite o expoente final: ")
 local fim = tonumber(io.read())
+
 print ("Digite a base: ")
 local base = tonumber(io.read())
 
-if inicio and fim and base then--vai garantir que so entramos valores numericos 
-    -- puxa a função
-    gerarTabelaPotencias(inicio, fim, base)
+if inicio and fim and base then -- vai garantir que so entramos valores numericos 
+    
+    gerarTabelaPotencias(inicio, fim, base) -- puxa a função
 else
-    print ("entrada invalida")
+    print ("entrada invalida") -- caso não entrar com valores numericos 
 end 
 

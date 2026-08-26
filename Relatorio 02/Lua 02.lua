5f97c6454537e1434eb51d9d84c248bc5b665837
Pedro Gabriel da Silva <p3dr0.tr@gmail.com>
2026-08-26 12:59:18 -0300

    formatei algumas coisas

--- Relatorio 02/Lua 02.lua
+++ Relatorio 02/Lua 02.lua
@@ -1,8 +1,8 @@
 function contarOcorrencias(tabela, alvo)
     local contador = 0
-    for i = 1, #tabela do --vasculha a tabela atraz do alvo 
+    for i = 1, #tabela do -- vasculha a tabela atraz do alvo 
         if tabela[i] == alvo then
-            contador = contador + 1 --contabiliza o alvo 
+            contador = contador + 1 -- contabiliza o alvo 
         end
     end 
     return contador            
@@ -12,20 +12,19 @@ print("Digite a quantidade de elementos (N): ")
 local N = tonumber(io.read())
 local tabela = {}
 
-if N and N > 0 then--garente que eu estou entrando com valores validos 
+if N and N > 0 then -- garente que eu estou entrando com valores validos 
     
-    --entrando com os valores da tabela 
-    for i = 1, N do    
+     
+    for i = 1, N do -- entrando com os elementos da tabela    
         print("Digite o elemento "..i..": ")
         local elemento = tonumber(io.read())
-        table.insert(tabela, elemento) --vai inserir o elemento na tabela 
+        table.insert(tabela, elemento) -- vai inserir os elemento dentro da tabela 
     end
-
-    --inserindo o valor a ser buscado 
-    print("Digite o número X a ser buscado: ")
+ 
+    print("Digite o número X a ser buscado: ") -- inserindo o valor a ser buscado
     local X = tonumber(io.read())
     
-    if X then--garante que eu estou entrando com valores validos
+    if X then -- garante que eu estou entrando com valores validos
         local repeticao = contarOcorrencias(tabela, X)
         print("O número "..X.." aparece "..repeticao.." vez(es) na tabela.")
     else
